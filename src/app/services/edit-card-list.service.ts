28df46707cfe6b857ffce32cda2125dba12559cd
i hate javascript <kotlikoffgg@gmail.com>
2024-05-15 13:39:53 +0200

    15.05 pt1

--- src/app/services/edit-card-list.service.ts
+++ src/app/services/edit-card-list.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
+export interface Property {
+  id: string;
+  name: string;
+  description: string;
+  icon: string;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +15,7 @@ export class EditCardListService {
   descriptionSubject = new BehaviorSubject<string>('');
   currentItemId: string = '';
   currentItemDescription: string = '';
-
+  currentpropertyList: Property[] = [];
   
   
   setDescription(description: string) {
