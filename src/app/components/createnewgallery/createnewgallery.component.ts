ccc1d7976b385329faa84966125c041d5992320e
i hate javascript <kotlikoffgg@gmail.com>
2024-05-15 14:43:58 +0200

    15.05 pt2

--- src/app/components/createnewgallery/createnewgallery.component.ts
+++ src/app/components/createnewgallery/createnewgallery.component.ts
@@ -4,32 +4,32 @@ import { Component } from '@angular/core';
 interface Card {
   name: string;
   id: string;
-  file: string;
+  file: File | null; // Заменяем string на File
+  imageUrl?: string;
 }
 
 @Component({
   selector: 'app-createnewgallery',
   templateUrl: './createnewgallery.component.html',
-  styleUrl: './createnewgallery.component.css'
+  styleUrls: ['./createnewgallery.component.css']
 })
 export class CreatenewgalleryComponent {
   gallery: Card[] = [];
   headerInput: string = "";
 
-  createCard() {
-    const newCard: Card = {
-      name: "Card_name",
-      id: Date.now().toString(),
-      file: "assets/avatar.png"
+  createCard(event: any) {
+    const fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      const file = fileList[0];
+      const newCard: Card = {
+        name: "Card_name",
+        id: Date.now().toString(),
+        file: file
+      }
+      this.gallery.push(newCard);
     }
-    this.gallery.push(newCard);
+    event.target.value = null;
   }
-
-  // onDrop(event: CdkDragDrop<List[]>) {
-  //     moveItemInArray(this.lists, event.previousIndex, event.currentIndex);
-    
-  // }
-
 
   onCardNameBlur(event: FocusEvent, card: Card) {
     const newValue = (event.target as HTMLInputElement).value;
@@ -39,4 +39,23 @@ export class CreatenewgalleryComponent {
   onHeaderBlur() {
     console.log(this.headerInput);
   }
+
+
+  getImageUrl(card: Card): string | null {
+  if (card.file) {
+    // Проверяем, был ли URL-адрес уже кэширован
+    if (card.imageUrl) {
+      return card.imageUrl;
+    } else {
+      const reader = new FileReader();
+      reader.onload = () => {
+        // Кэшируем URL-адрес изображения
+        card.imageUrl = reader.result as string;
+      };
+      reader.readAsDataURL(card.file);
+    }
+  }
+  return card.imageUrl ?? null;
+  }
+
 }
